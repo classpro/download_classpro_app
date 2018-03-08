277240a3b169b8d6a0d71b9472b1a3c8f38bf645
ARAVIND99 <aravind@classpro.in>
2018-03-08 12:31:21 +0530

    redirect to store

--- javascript/app.js
+++ javascript/app.js
@@ -1,24 +1,31 @@
 (function(){
+    var isRedirect = false;
     document.addEventListener('DOMContentLoaded', function(event) {
        console.log('content-loaded');
         var $playStore = document.getElementsByClassName('play-store')[0];
         var $appStore = document.getElementsByClassName('app-store')[0];
-        var isRedirect = false;
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
             if(navigator.userAgent.match(/android/i)) {
                 $appStore.style.display = 'none';
                 $playStore.style.marginRight = 'auto';
-                isRedirect = confirm('Open App on "Play Store"');
-                if(isRedirect) {
-                    window.location.href = "https://play.google.com/store/apps/details?id=com.classprostudentapp&hl=en";
-                }
+                setTimeout(redirectInAndroid, 1000);
             } else {
                 $playStore.style.display = 'none';
-                isRedirect = confirm('Open App on "App Store"');
-                if(isRedirect) {
-                    window.location.href = "https://itunes.apple.com/in/app/classpro/id1185807691?mt=8";
-                }
+                setTimeout(redirectInIos, 1000)
             }
         }
     });
+    var redirectInAndroid = function() {
+        isRedirect = confirm('Open App on "Play Store"');
+        if(isRedirect) {
+            window.location.href = "https://play.google.com/store/apps/details?id=com.classprostudentapp&hl=en";
+        }
+    };
+
+    var redirectInIos = function() {
+        isRedirect = confirm('Open App on "Play Store"');
+        if(isRedirect) {
+            window.location.href = "https://play.google.com/store/apps/details?id=com.classprostudentapp&hl=en";
+        }
+    }
 })();
